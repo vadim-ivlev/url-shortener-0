beb7a51c237157263504f768b7952ee99c8bd5ad
Vadim Ivlev <vadimivlev@Vadims-Laptop.local>
2024-07-01 16:06:18 +0100

    iter2: refactor.

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -2,33 +2,11 @@ package main
 
 import (
 	"fmt"
-	"net/http"
 
 	"github.com/vadim-ivlev/url-shortener/internal/server"
 )
 
 func main() {
-
 	fmt.Println("Server starting at http://localhost:8080/")
-
-	// Создать мультиплексор
-	mux := http.NewServeMux()
-
-	// Зарегистрировать обработчики
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == http.MethodPost {
-			server.ShortenURLHandler(w, r)
-		} else if r.Method == http.MethodGet {
-			server.RedirectHandler(w, r)
-		} else {
-			http.Error(w, "Invalid request method", http.StatusBadRequest)
-		}
-	})
-
-	// Запустить сервер на порту 8080
-	err := http.ListenAndServe(":8080", mux)
-	if err != nil {
-		panic(err)
-	}
-
+	server.Serve()
 }
--- internal/server/server.go
+++ internal/server/server.go
@@ -1,50 +1,32 @@
 package server
 
-import (
-	"io"
-	"net/http"
+import "net/http"
 
-	"github.com/vadim-ivlev/url-shortener/internal/urlshortener"
-)
+// addRouts добавляет обработчики маршрутов к мультиплексору.
+func addRouts(mux *http.ServeMux) {
+	// Зарегистрировать обработчики
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodPost {
+			ShortenURLHandler(w, r)
+		} else if r.Method == http.MethodGet {
+			RedirectHandler(w, r)
+		} else {
+			http.Error(w, "Invalid request method", http.StatusBadRequest)
+		}
+	})
 
-// ShortenURLHandler обрабатывает POST-запросы для создания короткого URL.
-func ShortenURLHandler(w http.ResponseWriter, r *http.Request) {
-	defer r.Body.Close()
-	body, err := io.ReadAll(r.Body)
-	if err != nil {
-		http.Error(w, "Internal server error", http.StatusInternalServerError)
-		return
-	}
-	originalURL := string(body)
-	if originalURL == "" {
-		http.Error(w, "Empty URL", http.StatusBadRequest)
-		return
-	}
-
-	// Сгенерировать короткий id и сохранить его
-	shortID := urlshortener.Us.Shorten(originalURL)
-	// Сгенерировать короткий URL
-	shortURL := "http://localhost:8080/" + shortID
-
-	w.WriteHeader(http.StatusCreated)
-	w.Header().Set("Content-Type", "text/plain")
-	w.Write([]byte(shortURL))
 }
 
-// RedirectHandler обрабатывает GET-запросы для перенаправления на оригинальный URL.
-func RedirectHandler(w http.ResponseWriter, r *http.Request) {
-	shortID := r.URL.Path[1:]
-	if shortID == "" {
-		http.Error(w, "Invalid request", http.StatusBadRequest)
-		return
-	}
+func Serve() {
+	// Создать мультиплексор
+	mux := http.NewServeMux()
 
-	// Получить оригинальный URL и перенаправить
-	originalURL := urlshortener.Us.Expand(shortID)
-	if originalURL == "" {
-		http.Error(w, "URL not found", http.StatusBadRequest)
-		return
-	}
+	// Зарегистрировать маршруты и обработчики
+	addRouts(mux)
 
-	http.Redirect(w, r, originalURL, http.StatusTemporaryRedirect)
+	// Запустить сервер на порту 8080
+	err := http.ListenAndServe(":8080", mux)
+	if err != nil {
+		panic(err)
+	}
 }
