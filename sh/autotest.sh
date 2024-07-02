79de13a3e72576f35f3f25c771b53949d62de76e
Vadim Ivlev <vadimivlev@Vadims-Laptop.local>
2024-07-02 02:15:28 +0100

    fix: autotests.sh.

--- sh/autotest.sh
+++ sh/autotest.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+go build -o cmd/shortener/shortener cmd/shortener/main.go
+
 shortenertest -test.v -test.run=^TestIteration1$ -binary-path=cmd/shortener/shortener
 shortenertest -test.v -test.run=^TestIteration2$ -binary-path=cmd/shortener/shortener
