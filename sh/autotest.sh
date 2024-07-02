2d274daf95776b527195c795972eb97bb2c8bfd1
Vadim Ivlev <vadimivlev@Vadims-Laptop.local>
2024-07-02 02:52:45 +0100

    fix: autotest.sh 2.

--- sh/autotest.sh
+++ sh/autotest.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# go build -o cmd/shortener/shortener cmd/shortener/main.go
+go build -o cmd/shortener/shortener cmd/shortener/main.go
+shortenertest -test.v -test.run=^TestIteration1$ -binary-path=cmd/shortener/shortener
+# shortenertest -test.v -test.run=^TestIteration2$ -binary-path=cmd/shortener/shortener -source-path=./internal
+shortenertest -test.v -test.run=^TestIteration2$ -source-path=.
 
-# shortenertest -test.v -test.run=^TestIteration1$ -binary-path=cmd/shortener/shortener
-# shortenertest -test.v -test.run=^TestIteration2$ -binary-path=cmd/shortener/shortener
-
-go build -buildvcs=false -o cmd/shortener/shortener cmd/shortener/main.go
-shortenertestbeta-darwin-arm64 -test.v -test.run=^TestIteration1$ -binary-path=cmd/shortener/shortener
-shortenertestbeta-darwin-arm64 -test.v -test.run=^TestIteration2$ -binary-path=cmd/shortener/shortener
+# go build -buildvcs=false -o cmd/shortener/shortener cmd/shortener/main.go
+# shortenertestbeta-darwin-arm64 -test.v -test.run=^TestIteration1$ -binary-path=cmd/shortener/shortener
+shortenertestbeta-darwin-arm64 -test.v -test.run=^TestIteration2$ -source-path=.
