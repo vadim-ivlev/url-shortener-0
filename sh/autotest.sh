e332e8be1a246b214d08ed689b8c435638e0acaf
Vadim Ivlev <vadimivlev@Vadims-Laptop.local>
2024-07-02 02:29:31 +0100

    fix: autotest.sh.

--- sh/autotest.sh
+++ sh/autotest.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-go build -o cmd/shortener/shortener cmd/shortener/main.go
+# go build -o cmd/shortener/shortener cmd/shortener/main.go
 
-shortenertest -test.v -test.run=^TestIteration1$ -binary-path=cmd/shortener/shortener
-shortenertest -test.v -test.run=^TestIteration2$ -binary-path=cmd/shortener/shortener
+# shortenertest -test.v -test.run=^TestIteration1$ -binary-path=cmd/shortener/shortener
+# shortenertest -test.v -test.run=^TestIteration2$ -binary-path=cmd/shortener/shortener
+
+go build -buildvcs=false -o cmd/shortener/shortener cmd/shortener/main.go
+shortenertestbeta-darwin-arm64 -test.v -test.run=^TestIteration1$ -binary-path=cmd/shortener/shortener
+shortenertestbeta-darwin-arm64 -test.v -test.run=^TestIteration2$ -binary-path=cmd/shortener/shortener
