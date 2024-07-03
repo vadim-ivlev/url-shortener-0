4aa37aa58b94f9ce0ea61eaddef4ceebb2c3c354
Vadim Ivlev <vadimivlev@Vadims-Laptop.local>
2024-07-03 16:13:22 +0100

    fix: cleaning iter3.

--- internal/server/server.go
+++ internal/server/server.go
@@ -7,34 +7,6 @@ import (
 	"github.com/vadim-ivlev/url-shortener/internal/handlers"
 )
 
-// addRouts добавляет обработчики маршрутов к мультиплексору.
-func addRouts(mux *http.ServeMux) {
-	// Зарегистрировать обработчики
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == http.MethodPost {
-			handlers.ShortenURLHandler(w, r)
-		} else if r.Method == http.MethodGet {
-			handlers.RedirectHandler(w, r)
-		} else {
-			http.Error(w, "Invalid request method", http.StatusBadRequest)
-		}
-	})
-}
-
-func Serve() {
-	// Создать мультиплексор
-	mux := http.NewServeMux()
-
-	// Зарегистрировать маршруты и обработчики
-	addRouts(mux)
-
-	// Запустить сервер на порту 8080
-	err := http.ListenAndServe(":8080", mux)
-	if err != nil {
-		panic(err)
-	}
-}
-
 // Using Chi router
 func ServeChi() {
 	r := chi.NewRouter()
--- sh/autotest.sh
+++ sh/autotest.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+echo "Looking for dead code ---------------------------"
+# go install github.com/deadcode/deadcode@latest
+deadcode ./...
+
+echo "Test coverage -----------------------------------"
+go test -cover ./...
+# go test -coverprofile=cover.out ./...
+# go tool cover -html=cover.out
+
+echo "Running tests -----------------------------------"
+go test -count=1 ./...
+
+
 echo "Building shortenertest ---------------------------"
 go build -o cmd/shortener/shortener cmd/shortener/main.go
 
