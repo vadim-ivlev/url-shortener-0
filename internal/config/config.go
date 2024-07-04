b516f47435aacaf09da7115e1dd86b3dc08cad71
Vadim Ivlev <vadimivlev@Vadims-Laptop.local>
2024-07-04 13:56:50 +0100

    add: reading env variables.

--- internal/config/config.go
+++ internal/config/config.go
@@ -24,15 +24,37 @@ package config
 import (
 	"flag"
 	"fmt"
+
+	"github.com/caarlos0/env/v11"
 )
+
+type envConfig struct {
+	ServerAddress string `env:"SERVER_ADDRESS"`
+	BaseURL       string `env:"BASE_URL"`
+}
 
 var Address string
 var BaseURL string
 
 func ParseCommandLine() {
+	// Читаем параметры командной строки с значениями по умолчанию
 	flag.StringVar(&Address, "a", "localhost:8080", "HTTP server address")
 	flag.StringVar(&BaseURL, "b", "http://localhost:8080", "Base URL")
 	flag.Parse()
+
+	// Читаем переменные окружения
+	cfg := envConfig{}
+	if err := env.Parse(&cfg); err != nil {
+		fmt.Printf("%+v\n", err)
+	}
+
+	// Если переменные окружения заданы, то используем их
+	if cfg.ServerAddress != "" {
+		Address = cfg.ServerAddress
+	}
+	if cfg.BaseURL != "" {
+		BaseURL = cfg.BaseURL
+	}
 
 	fmt.Println("Server Address:", Address)
 	fmt.Println("Shortened Base URL:", BaseURL)
